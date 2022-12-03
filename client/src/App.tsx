924f03c5fda89504d4e34b0869fd752229cd05de
AdneCode <super-gijs@hotmail.com>
2022-12-04 00:47:40 +0100

    feat: app.js

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const API_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:4000';
+    const [gameCode, setGameCode] = useState<string | null>(null);
+    const [gameCodeInp, setGameCodeInp] = useState<string>('');
+    const requestGame = async () => {
+        const idResponse = await axios.get(`${API_URL}/game`);
+        setGameCode(idResponse.data.id);
+    };
+
+    return (
+        <>
+            <button onClick={() => requestGame()}>Start new game</button>
+            <div>
+                <input
+                    value={gameCodeInp}
+                    onChange={(e) => setGameCodeInp(e.target.value)}
+                />
+                <button onClick={() => setGameCode(gameCodeInp)}>
+                    Join game
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default App;
