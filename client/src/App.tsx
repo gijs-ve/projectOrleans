8a6146cb03ea1fbe4f760f80830415d9b7a6342c
AdneCode <super-gijs@hotmail.com>
2022-12-04 00:54:15 +0100

    fix: adressed useStates in app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     const API_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:4000';
     const [gameCode, setGameCode] = useState<string | null>(null);
-    const [gameCodeInp, setGameCodeInp] = useState<string>('');
+    const [gameId, setGameId] = useState<string>('');
     const requestGame = async () => {
         const idResponse = await axios.get(`${API_URL}/game`);
         setGameCode(idResponse.data.id);
@@ -17,12 +16,10 @@ function App() {
             <button onClick={() => requestGame()}>Start new game</button>
             <div>
                 <input
-                    value={gameCodeInp}
-                    onChange={(e) => setGameCodeInp(e.target.value)}
+                    value={gameId}
+                    onChange={(e) => setGameId(e.target.value)}
                 />
-                <button onClick={() => setGameCode(gameCodeInp)}>
-                    Join game
-                </button>
+                <button onClick={() => setGameCode(gameCode)}>Join game</button>
             </div>
         </>
     );
