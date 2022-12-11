b45f458d546b8a4bbc6a46942f8d660f7859fb0d
AdneCode <super-gijs@hotmail.com>
2022-12-11 10:28:35 +0100

    fix: useEffect for sendRoom

--- client/src/components/buttons/CreateRoomButton.tsx
+++ client/src/components/buttons/CreateRoomButton.tsx
@@ -1,19 +1,23 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { SocketContext } from '../../socket/socket';
 import { Data } from '../../../../types/types';
-import { useDispatch } from 'react-redux';
+import { useAppDispatch } from '../../hooks/hooks';
 import { gameReducer } from '../../store';
 
 const CreateRoomButton = () => {
     const socket = useContext(SocketContext);
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
     const createRoom = () => {
         const data: Data = { roomName: 'Test' };
         socket.emit('createRoom', data);
     };
-    socket.on('sendRoom', (data: Data) => {
-        dispatch(gameReducer({ type: 'GAME_RECEIVED', game: data.room }));
-    });
+    useEffect(() => {
+        socket.on('sendRoom', (data: Data) => {
+            dispatch(gameReducer({ type: 'GAME_RECEIVED', game: data.room }));
+            dispatch(gameReducer({ type: 'SET_CONNECTED' }));
+        });
+    }, []);
+
     return <button onClick={() => createRoom()}>Create game</button>;
 };
 
