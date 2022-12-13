7203181e22b397b54b05d020fb3870cce0361342
AdneCode <super-gijs@hotmail.com>
2022-12-13 19:19:31 +0100

    feat: commentary

--- server/roomSystem/socketIdIsHost.ts
+++ server/roomSystem/socketIdIsHost.ts
@@ -4,9 +4,10 @@ export const socketIdIsHost = (
     roomId: string,
     socketId: string,
 ) => {
+    //find the room first that is requested
     const room = rooms.find((i: Room) => {
         return i.id === roomId;
     });
-
+    //return true or false whether the hostId is equal to the socketId
     return room.hostId === socketId;
 };
