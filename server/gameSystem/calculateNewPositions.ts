2ca7f72c2cde02ce16baff39fd40e3289160bc1d
AdneCode <super-gijs@hotmail.com>
2022-12-13 19:22:34 +0100

    feat: playerCount

--- server/gameSystem/calculateNewPositions.ts
+++ server/gameSystem/calculateNewPositions.ts
@@ -3,6 +3,7 @@ import { generateSpawnTable } from './generateSpawnTable';
 export const calculateNewPositions = (room: Room) => {
     if (room.round === 1) {
         console.log('5 ROOM', room);
-        return generateSpawnTable(room.arena, room.size, false);
+        const spawnTable = generateSpawnTable(room.arena, room.size, false);
+        const playerCount = room.players.length;
     }
 };
