563f39e0b0e406599642876ebd890e3760545448
AdneCode <super-gijs@hotmail.com>
2022-12-13 20:00:35 +0100

    feat: providePositions

--- server/gameSystem/calculateNewPositions.ts
+++ server/gameSystem/calculateNewPositions.ts
@@ -1,9 +1,10 @@
 import { Room } from '../../types/types';
 import { generateSpawnTable } from './generateSpawnTable';
+import { providePositions } from './providePositions';
 export const calculateNewPositions = (room: Room) => {
     if (room.round === 1) {
         console.log('5 ROOM', room);
         const spawnTable = generateSpawnTable(room.arena, room.size, false);
-        const playerCount = room.players.length;
+        const newPlayers = providePositions(spawnTable, room.players);
     }
 };
---
+++ server/gameSystem/providePositions.ts
@@ -0,0 +1,6 @@
+import { Players, Square } from '../../types/types';
+
+//takes a spawnTable and a playersTable, returns a new playersTable with spawn positions included
+export const providePositions = (spawnTable: Square[], players: Players) => {
+    const playerCount = players.length;
+};
