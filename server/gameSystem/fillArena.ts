2f968084d78f4a69b4172607f6751f73d5d97822
AdneCode <super-gijs@hotmail.com>
2023-01-09 23:50:53 +0100

    fix: arena

--- server/gameSystem/fillArena.ts
+++ server/gameSystem/fillArena.ts
@@ -5,14 +5,14 @@ import { Room, Rooms, Arena, Square } from '../../types/types';
 export const fillArena = (rooms: Rooms, room: Room) => {
     const newRoom = room;
     const arenaSize = room.size;
-    const arena: Arena = [{ x: 0, y: 0 }];
+    const newArena: Arena = [{ x: 0, y: 0 }];
     for (let x = 1; x < arenaSize + 1; x++) {
         for (let y = 1; y < arenaSize + 1; y++) {
             const square: Square = { x, y };
-            arena.push(square);
+            newArena.push(square);
         }
     }
-    newRoom.arena = arena;
+    newRoom.arena = newArena;
     const newRooms = generateNewRooms(rooms, newRoom);
     return { newRooms, newRoom };
 };
