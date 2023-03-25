d4a1978cb42030befca88adbc6a9c52654e3dce9
AdneCode <super-gijs@hotmail.com>
2023-03-25 19:36:50 +0100

    fix: actual find

--- server-rework/roomSystem/findRoomById.ts
+++ server-rework/roomSystem/findRoomById.ts
@@ -1,4 +1,7 @@
-import { Rooms, Room } from '../../types/types';
-export const findRoomById = (rooms: Rooms, roomId: string) => {
+import { Room, Rooms } from '../../types/types';
+
+import store from 'store';
+export const findRoomById = (roomId: string) => {
+    const { rooms } = store.getState().roomState;
     return rooms.find((i: Room) => i.id === roomId);
 };
--- server-rework/roomSystem/findRoomBySocketId.ts
+++ server-rework/roomSystem/findRoomBySocketId.ts
@@ -1,8 +1,11 @@
-import { Room } from "../../types/types"
-import { roomHasSocketId } from "./roomHasSocketId"
+import { Room } from '../../types/types';
+import { roomHasSocketId } from './roomHasSocketId';
+import store from 'store';
+
 //return the room the socketId is in that is provided
-export const findRoomBySocketId = (rooms, socketId) => {
-    return rooms.map((i: Room) => {
-        return (roomHasSocketId(i, socketId))
-    })
-}
+export const findRoomBySocketId = (socketId: string): Room => {
+    const { rooms } = store.getState().roomState;
+    return rooms.find((i: Room) => {
+        return roomHasSocketId(i, socketId);
+    });
+};
