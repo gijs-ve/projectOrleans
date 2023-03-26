34e21d6f4707142d66f356985041b61a150f611f
AdneCode <super-gijs@hotmail.com>
2023-03-26 20:16:15 +0200

    fix: config

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -1,4 +1,5 @@
 //export const apiUrl = `http://192.168.1.121:4000`;
 //export const apiUrl = `http://192.168.1.85:4000`;
 //export const apiUrl = `http://192.168.1.76:4000`;
-export const apiUrl = `http://192.168.0.118:4000`;
+// export const apiUrl = `http://192.168.0.118:4000`;
+export const apiUrl = '80.112.165.40:4000';
