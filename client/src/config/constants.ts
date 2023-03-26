a8f345ef1ae099e45034e48232bd4f75920e272f
AdneCode <super-gijs@hotmail.com>
2023-03-26 21:03:25 +0200

    config

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -2,4 +2,4 @@
 //export const apiUrl = `http://192.168.1.85:4000`;
 //export const apiUrl = `http://192.168.1.76:4000`;
 // export const apiUrl = `http://192.168.0.118:4000`;
-export const apiUrl = 'https://3e46-80-112-165-40.eu.ngrok.io ';
+export const apiUrl = 'http://dc30-80-112-165-40.eu.ngrok.io';
